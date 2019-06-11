60c64fc1b645be7cfaf934ac81170642d21b0b91
FiveChjr <f5.chjr@gmail.com>
2019-06-11 20:12:13 +0700

    [Components] Fix

--- components/sections/footer.jsx
+++ components/sections/footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => (
     <footer className="py-10 bg-white">
         <div className="mx-8">
             <div className="container">
-                <p className="text-xs font-normal text-center text-black leading-relaxed tracking-widest">
+                <p className="text-xs font-normal text-center text-black mb-3 tracking-widest">
                     &copy; THINC. 2019
                 </p>
                 <p className="text-xs font-normal text-center text-gray-600 leading-none tracking-widest">
