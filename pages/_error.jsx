6751bd1d3a9399f0dc5568aa983b5101c497fd2a
FiveChjr <f5.chjr@gmail.com>
2019-06-12 09:15:59 +0700

    [Project] Add 404 page

---
+++ pages/_error.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Error extends React.Component {
+    static getInitialProps({ res, err }) {
+        const statusCode = res ? res.statusCode : err ? err.statusCode : null
+        return { statusCode }
+    }
+
+    render() {
+        // return (
+        //     <p>
+        //         {this.props.statusCode
+        //             ? `An error ${this.props.statusCode} occurred on server`
+        //             : 'An error occurred on client'}
+        //     </p>
+        // )
+        return <img src="/static/404.jpg" alt={500} />
+    }
+}
+
+export default Error
