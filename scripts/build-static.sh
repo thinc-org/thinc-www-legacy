d3dba1e72807356031168c45a8f0592cfc7f8140
FiveChjr <f5.chjr@gmail.com>
2019-06-13 23:17:43 +0700

    [Project] Change `now.json` to `later.json` to disable Now

--- scripts/build-static.sh
+++ scripts/build-static.sh
@@ -6,4 +6,4 @@ yarn build
 yarn export
 touch out/.nojekyll
 git add out && git commit -m '[Build] Build and commit from `build-static.sh`'
-git subtree push --prefix out origin gh-pages
+git subtree push --prefix out origin gh-pages-deploy
