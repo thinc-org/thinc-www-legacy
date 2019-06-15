074311131e366574655a8b6a548c195023480e00
new5558 <new17353@gmail.com>
2019-06-15 12:45:31 +0700

    [Components] Add meta for SEO

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,7 +9,15 @@ class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Head />
+                <Head>
+                    <meta name="google-site-verification" content="Rdku_VJ2ryBQB6Uh0Ti5j4RgcN-Y2X60HwFvFur-Llo" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <meta name="description" content="Thinc. is a student-run community aiming to make impact to society. A student community committed to promoting real-world profession skills with the goal of incubating the leader of tomorrow." />
+                    <meta name="keywords" content="Chula Pop Bus, Thinc, วิศวะคอมพ์จุฬา, ise chula, chulalongkorn, วิศวะจุฬา, open registration, cu blood" />
+                    <meta httpEquiv="content-language" content="th,en" />
+                    <meta name="robots" content="index, follow" />
+                    <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+                </Head>
                 <body className="font-sans leading-normal text-base text-black antialiased">
                     <Main />
                     <NextScript />
