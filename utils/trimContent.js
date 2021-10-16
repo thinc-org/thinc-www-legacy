d15c63789c078aabb9bd63001af63358bb096047
new5558 <new17353@gmail.com>
2021-10-16 22:16:18 +0700

    docs: create jsdocs for trimContent function

--- utils/trimContent.js
+++ utils/trimContent.js
@@ -1,16 +1,22 @@
+/**
+ *
+ * @param {string} content : content from RSS content:encodedSnippet;
+ * @returns trimmed content to first line and not more than 100 characters
+ */
 const trimContent = (content) => {
   const firstLine = content.split('\n')[0]
-  let textCount = 0
+  const CHARECTER_LIMIT = 100
+  let charCount = 0
   let result = ''
   let endOfLine = true
   const array = firstLine.split(' ')
   for (let i = 0; i < array.length; i++) {
     const word = array[i]
-    if (textCount >= 100) {
+    if (charCount >= CHARECTER_LIMIT) {
       endOfLine = false
       break
     }
-    textCount += word.length
+    charCount += word.length
     result += ` ${word}`
   }
 
