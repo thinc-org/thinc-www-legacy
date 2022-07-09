9e9bd8ee92ea7e1e53a9ad8a9e143f3950b714b1
betich <betich365@gmail.com>
2022-07-09 10:22:12 +0700

    feat: change generation increase date to 1st july

--- src/components/announcements/cuGeneration.tsx
+++ src/components/announcements/cuGeneration.tsx
@@ -5,7 +5,7 @@ import IncrementalNumber from './incrementalNumber'
 const CUGeneration = () => {
   // Assume: increase generation every 31st July of every year
   // Note: Javascript count January as 0
-  const currentCUGeneration = differenceInYears(new Date(), new Date(1916, 6, 31))
+  const currentCUGeneration = differenceInYears(new Date(), new Date(1916, 6, 1))
   return <IncrementalNumber start={1} end={currentCUGeneration} />
 }
 
--- src/components/announcements/thincGeneration.tsx
+++ src/components/announcements/thincGeneration.tsx
@@ -4,7 +4,7 @@ import ordinal from 'ordinal'
 const ThincGeneration = () => {
   // Assume: increase generation every 31st July of every year
   // Note: Javascript count January as 0
-  const currentCUGeneration = differenceInYears(new Date(), new Date(1916, 6, 31))
+  const currentCUGeneration = differenceInYears(new Date(), new Date(1916, 6, 1))
   const currentThincGeneration = currentCUGeneration - 96
   return <span>{ordinal(currentThincGeneration)}</span>
 }
